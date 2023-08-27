1db4de48168cea2a003a19411b4a6663b95a0a3a
Shubham Waje <waje.shubham111@gmail.com>
2023-08-27 17:45:13 +0530

    add function to format the code snippet using prettier format api

--- src/components/templates/authentication/register.jsx
+++ src/components/templates/authentication/register.jsx
@@ -1,7 +1,15 @@
+import Button from "@/components/common/Button";
+import { LifebuoyIcon } from "@heroicons/react/20/solid";
 import React from "react";
 
 const Register = () => {
-  return <div className="text-red-500 font-bold p-6">Register</div>;
+  return (
+    <div>
+      <div className="text-red-500 font-bold p-6">Register</div>
+      <div className="text-red-500 font-bold p-6">Register</div>
+      <div className="text-red-500 font-bold p-6">Register</div>
+    </div>
+  );
 };
 
 export default Register;
