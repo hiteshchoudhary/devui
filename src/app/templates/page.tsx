04e9590f509487ea3a8250c9fee9c7131a23b6b8
Shubham Waje <waje.shubham111@gmail.com>
2023-09-09 23:54:07 +0530

    refactor(devui): redesigned templates screen padding

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -13,7 +13,7 @@ const Templates = () => {
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {applications.map((application, i) => {
         return (
-          <main key={i} className="px-4 py-6">
+          <main key={i} className="py-6 md:px-4">
             <div className="mx-auto grid max-w-4xl grid-cols-1">
               <div className="relative col-start-1 row-start-1 flex flex-col-reverse bg-gradient-to-t from-black/100 via-black/0 p-3">
                 <h1 className="mt-1 text-lg font-semibold text-white">
