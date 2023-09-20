27157b193736964c84976608c2f60fd3801bd595
Deepesh Gupta <deepesh554.dg@gmail.com>
2023-09-20 15:05:19 +0530

    refactor(youtube): profile video list responseive bug fix

--- src/components/templates/youtube/profile-video-list.jsx
+++ src/components/templates/youtube/profile-video-list.jsx
@@ -129,7 +129,7 @@ const ProfileVideoList = () => {
               </div>
             </div>
 
-            <ul className="no-scrollbar sticky top-[66px] z-40 flex flex-row gap-x-2 overflow-auto border-b-2 border-gray-400 bg-[#121212] py-2 sm:top-[82px]">
+            <ul className="no-scrollbar sticky top-[66px] z-[2] flex flex-row gap-x-2 overflow-auto border-b-2 border-gray-400 bg-[#121212] py-2 sm:top-[82px]">
               <li className="w-full">
                 <button className="w-full border-b-2 border-[#ae7aff] bg-white px-3 py-1.5 text-[#ae7aff]">
                   Videos
