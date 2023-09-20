0e76b29a056100702bae95d1aba83c7393d45407
Deepesh Gupta <deepesh554.dg@gmail.com>
2023-09-20 14:49:52 +0530

    refactor(youtube): responsive of profile videolist page

--- src/components/templates/youtube/profile-video-list.jsx
+++ src/components/templates/youtube/profile-video-list.jsx
@@ -88,7 +88,7 @@ const ProfileVideoList = () => {
           </ul>
         </aside>
         <section className="w-full pb-[70px] sm:ml-[70px] sm:pb-0 lg:ml-0">
-          <div className="relative w-full pt-[16.28%]">
+          <div className="relative min-h-[150px] w-full pt-[16.28%]">
             <div className="absolute inset-0 overflow-hidden">
               <img
                 src="https://images.pexels.com/photos/1092424/pexels-photo-1092424.jpeg?auto=compress"
@@ -97,51 +97,56 @@ const ProfileVideoList = () => {
             </div>
           </div>
           <div className="px-4 pb-4">
-            <div className="flex -translate-y-6 flex-wrap justify-between gap-4 md:flex-nowrap">
-              <div className="inline-flex gap-4">
-                <span className="inline-block h-28 w-28 overflow-hidden rounded-full border-2">
-                  <img
-                    src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-in-headphones-showing-programming-process-on-a-laptop.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="Profile"
-                    className="h-full w-full"
-                  />
-                </span>
-                <div className="first-letter inline-block pt-11">
-                  <h1 className="font-bolg text-xl">React Patterns</h1>
-                  <p className="text-sm text-gray-400">@reactpatterns</p>
-                  <p className="text-sm text-gray-400">
-                    600k Followers&nbsp;&middot;&nbsp;220 Following
-                  </p>
+            <div className="flex flex-wrap gap-4 pb-4 pt-6">
+              <span className="relative -mt-12 inline-block h-28 w-28 shrink-0 overflow-hidden rounded-full border-2">
+                <img
+                  src="https://images.pexels.com/photos/1115816/pexels-photo-1115816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="Profile"
+                  className="h-full w-full"
+                />
+              </span>
+
+              <div className="mr-auto inline-block">
+                <h1 className="font-bolg text-xl">React Patterns</h1>
+                <p className="text-sm text-gray-400">@reactpatterns</p>
+                <p className="text-sm text-gray-400">
+                  600k Followers&nbsp;&middot;&nbsp;220 Following
+                </p>
+              </div>
+
+              <div className="inline-block">
+                <div className="inline-flex min-w-[130px] justify-end">
+                  <button className="group/btn mr-1 flex w-full items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
+                    <span className="inline-block w-5">
+                      <UserPlusIcon strokeWidth={2} />
+                    </span>
+                    <span className="group-focus/btn:hidden">Follow</span>
+                    <span className="hidden group-focus/btn:block">
+                      Unfollow
+                    </span>
+                  </button>
                 </div>
               </div>
-              <div className="inline-block pt-11">
-                <button className="group/btn mr-1 flex w-full items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
-                  <span className="inline-block w-5">
-                    <UserPlusIcon strokeWidth={2} />
-                  </span>
-                  <span className="group-focus/btn:hidden">Follow</span>
-                  <span className="hidden group-focus/btn:block">Unfollow</span>
-                </button>
-              </div>
             </div>
-            <ul className="sticky top-[66px] z-40 flex flex-row gap-x-2 border-b border-gray-600 bg-[#121212] py-2 sm:top-[82px]">
+
+            <ul className="no-scrollbar sticky top-[66px] z-40 flex flex-row gap-x-2 overflow-auto border-b-2 border-gray-400 bg-[#121212] py-2 sm:top-[82px]">
               <li className="w-full">
-                <button className="w-full bg-white px-3 py-1.5 text-[#ae7aff]">
+                <button className="w-full border-b-2 border-[#ae7aff] bg-white px-3 py-1.5 text-[#ae7aff]">
                   Videos
                 </button>
               </li>
               <li className="w-full">
-                <button className="w-full px-3 py-1.5 text-gray-400">
+                <button className="w-full border-b-2 border-transparent px-3 py-1.5 text-gray-400">
                   Playlist
                 </button>
               </li>
               <li className="w-full">
-                <button className="w-full px-3 py-1.5 text-gray-400">
+                <button className="w-full border-b-2 border-transparent px-3 py-1.5 text-gray-400">
                   Tweets
                 </button>
               </li>
               <li className="w-full">
-                <button className="w-full px-3 py-1.5 text-gray-400">
+                <button className="w-full border-b-2 border-transparent px-3 py-1.5 text-gray-400">
                   Followings
                 </button>
               </li>
