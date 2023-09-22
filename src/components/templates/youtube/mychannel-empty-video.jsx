434f689a539ba0e7390e0a3e68e8b129995f8e9b
Deepesh Gupta <deepesh554.dg@gmail.com>
2023-09-22 10:53:02 +0530

    refactor(youtube): button alignment fixed

--- src/components/templates/youtube/mychannel-empty-video.jsx
+++ src/components/templates/youtube/mychannel-empty-video.jsx
@@ -117,14 +117,12 @@ const MyChannelEmptyVideo = () => {
               </div>
 
               <div className="inline-block">
-                <div className="inline-flex min-w-[145px] justify-end">
-                  <button className="group/btn mr-1 flex w-full items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
-                    <span className="inline-block w-5">
-                      <PencilIcon strokeWidth={2} />
-                    </span>
-                    Edit
-                  </button>
-                </div>
+                <button className="group/btn mr-1 flex w-full items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
+                  <span className="inline-block w-5">
+                    <PencilIcon strokeWidth={2} />
+                  </span>
+                  Edit
+                </button>
               </div>
             </div>
 
