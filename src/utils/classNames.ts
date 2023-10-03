df5cad16c52252f969f77614b1c3ba273f0a5378
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2023-10-03 12:54:38 +0530

    removed extra files

--- src/utils/classNames.ts
+++
@@ -1,5 +0,0 @@
-const classNames = (...classes: string[]) => {
-  return classes.filter(Boolean).join(" ");
-};
-
-export default classNames;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,10 @@
-import renderHTMLFromJSX from "./renderHTMLFromJsx";
-import classNames from "./classNames";
+import { ReactElement } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
 
-export { renderHTMLFromJSX, classNames };
+export const classNames = (...classes: string[]) => {
+  return classes.filter(Boolean).join(" ");
+};
+
+export const renderHTMLFromJSX = (jsx: ReactElement): string => {
+  return renderToStaticMarkup(jsx).replaceAll("&amp;", "&");
+};
