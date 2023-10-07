3c58ec9eb96b2d5df164668583d15a25155d149a
Deepesh Gupta <deepesh554.dg@gmail.com>
2023-10-07 11:44:54 +0530

    refactor(youtube): added profile icon on necessary page in header

--- src/components/templates/youtube/uploaded-video.jsx
+++ src/components/templates/youtube/uploaded-video.jsx
@@ -58,12 +58,18 @@ const UploadedVideo = () => {
                 ))}
             </ul>
 
-            <div className="mb-8 mt-auto flex w-full flex-wrap gap-4 px-4 sm:mb-0 sm:mt-0 sm:items-center sm:px-0">
-              <button className="w-full bg-[#383737] px-3 py-2 hover:bg-[#4f4e4e] sm:w-auto sm:bg-transparent">
-                Log in
-              </button>
-              <button className="mr-1 w-full bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
-                Sign up
+            <div className="mb-8 mt-auto px-4 sm:mb-0 sm:mt-0 sm:px-0">
+              <button className="flex w-full gap-4 text-left sm:items-center">
+                <img
+                  src="https://images.pexels.com/photos/1115816/pexels-photo-1115816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="React-Patterns"
+                  className="h-16 w-16 shrink-0 rounded-full sm:h-12 sm:w-12"
+                />
+
+                <div className="w-full pt-2 sm:hidden">
+                  <h6 className="font-semibold">React Patterns</h6>
+                  <p className="text-sm text-gray-300">@reactpatterns</p>
+                </div>
               </button>
             </div>
           </div>
